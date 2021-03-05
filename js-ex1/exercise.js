21c098484f44e17599f910340bba5b02bbbfe0f8
zamanbhutta <marham.ayat@gmail.com>
2021-03-05 15:20:29 +0100

    js-ex 1

--- js-ex1/exercise.js
+++ js-ex1/exercise.js
@@ -1,17 +1,17 @@
 arr (["banana", "apple", 5, 6, 7 , "orange" , 9, 8]);
 arr (["banana", "apple", 5, 6, 7 , "orange", 5, "kiwi"]);
 function arr(array) {
-    var countNumbers = 0
-    var countLetters= 0
+    var countNumbers = 0;
+    var countWords= 0;
     array.forEach( function(item) {
         if (typeof item === "number"){
-        countNumbers = countNumbers +1 ;
+        countNumbers = countNumbers +1;
         } else {
-            countLetters = countLetters + 1;
+            countWords = countWords + 1;
         }
         });  
 console.log(`${array.length} elements in your array.`);
 console.log(`${countNumbers}  numbers`);
-console.log(`${countLetters} letters`);
+console.log(`${countWords} words`);
 };
 
